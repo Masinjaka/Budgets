645b49981397345b1a97c0aa57b1351633ab1302
Masinjaka <amasinjaka@gmail.com>
2025-06-07 10:59:05 +0300

    refactor home page layout and add custom navigation bar

--- lib/pages/profile/page/profile_page.dart
+++ lib/pages/profile/page/profile_page.dart
@@ -1,5 +1,9 @@
+import 'package:budgets/provider/auth_provider.dart';
+import 'package:budgets/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class ProfilePage extends ConsumerStatefulWidget {
   const ProfilePage({super.key});
@@ -10,8 +14,31 @@ class ProfilePage extends ConsumerStatefulWidget {
 
 class _ProfilePageState extends ConsumerState<ProfilePage> {
 
+  bool _isLoading = false;
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext buildcontext) {
+    return Scaffold(
+      bottomNavigationBar: _buildSignoutButton(),
+    );
+  }
+
+  Padding _buildSignoutButton() {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.w),
+        child: CustomButton(
+          text: 'Se déconnecter',
+          onPressed: () async {
+            // start authenticating
+            setState(() => _isLoading = true);
+            await ref.read(authProvider.notifier).signOut();
+            if(!mounted) return;
+            context.go('/login');
+            setState(() => _isLoading = false);
+            // end authenticating
+          },
+          isLoading: _isLoading,
+        ),
+      );
   }
 }
